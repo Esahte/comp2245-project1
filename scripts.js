df9cfea4839f479ee128ec36b903020b23cda4b3
Esahtengang Asonganyi <93486453+Esahte@users.noreply.github.com>
2023-10-31 14:26:54 -0400

    Added all the needed code for the styles.css and scripts.js files completed assignment

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 /* Add your JavaScript to this file */
+"use strict";
+
+window.onload = function() {
+    // Add event listener to the subscription button
+    document.getElementsByClassName('btn')[1].addEventListener('click', event => {
+        // Prevent the default behaviour of the button
+        event.preventDefault();
+        // Get the email address
+        let email = document.getElementById('email').value;
+        // Check if the email address is empty
+        if (email === '') {
+            document.getElementsByClassName('message')[0].innerHTML = 'Please enter a valid email address';
+            document.getElementsByClassName('message')[0].style.display = 'block';
+        } else {
+            document.getElementsByClassName('message')[0].innerHTML = `Thank you! Your email address ${email} has been added to our mailing list!`;
+            document.getElementsByClassName('message')[0].style.display = 'block';
+        }
+    });
+}
